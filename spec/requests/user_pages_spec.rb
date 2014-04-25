1c5310ccfe7df57e6bc1c350c8ec86a26831b0f2
anantalft <anantalamichhane@lftechnology.com>
2014-04-25 16:50:27 +0545

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,7 +18,15 @@ describe "User pages" do
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
+  describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
 
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+      
   describe "signup" do
 
     before { visit signup_path }
